20faaea35285dff9fa1446a474f3dc2bf5d19441
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2025-09-25 11:55:35 +0700

    refactor: add container component

--- src/components/common/Container/index.tsx
+++ src/components/common/Container/index.tsx
@@ -1,16 +1,22 @@
 import React, { PropsWithChildren } from 'react';
 
-import { ArrayStringTypes, ClassnameTypes } from '@/libs/@types';
+import { ArrayStringTypes } from '@/libs/@types';
 import { joinArrayString } from '@/libs/utils';
 
-export type ContainerTypes = PropsWithChildren & ClassnameTypes;
+export type ContainerTypes = PropsWithChildren & React.HTMLAttributes<HTMLElement>;
 
-const Container = ({ className, children }: ContainerTypes): React.ReactElement => {
+const Container = ({ className, children, ...props }: ContainerTypes): React.ReactElement => {
     let containerClass: ArrayStringTypes = ['container'];
     if (className) containerClass.push(className);
     containerClass = joinArrayString(containerClass);
 
-    return <div className={containerClass}>{children}</div>;
+    return (
+        <div
+            className={containerClass}
+            {...props}>
+            {children}
+        </div>
+    );
 };
 
 export default Container;
