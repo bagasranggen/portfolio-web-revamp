7df5f2725d9436407771470bab177ece977daca8
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2025-09-25 13:02:09 +0700

    refactor: update classes

--- src/components/common/Container/index.tsx
+++ src/components/common/Container/index.tsx
@@ -3,10 +3,11 @@ import React, { PropsWithChildren } from 'react';
 import { ArrayStringTypes } from '@/libs/@types';
 import { joinArrayString } from '@/libs/utils';
 
-export type ContainerTypes = PropsWithChildren & React.HTMLAttributes<HTMLElement>;
+export type ContainerTypes = PropsWithChildren<{ fluid?: boolean }> & React.HTMLAttributes<HTMLElement>;
 
-const Container = ({ className, children, ...props }: ContainerTypes): React.ReactElement => {
-    let containerClass: ArrayStringTypes = ['container'];
+const Container = ({ className, children, fluid, ...props }: ContainerTypes): React.ReactElement => {
+    let containerClass: ArrayStringTypes = [];
+    containerClass.push(fluid ? 'container-fluid' : 'container');
     if (className) containerClass.push(className);
     containerClass = joinArrayString(containerClass);
 
