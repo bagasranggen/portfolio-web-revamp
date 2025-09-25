2e9afcc649c2070ba8a691987fba43bc2a573efb
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2025-09-25 11:56:41 +0700

    refactor: add helper and common types

--- src/libs/@types/common.ts
+++ src/libs/@types/common.ts
@@ -1,3 +1,11 @@
+export type Component<Props> = {
+    (props: Props): React.ReactElement | null;
+};
+
+export type RefComponent<Props, Element> = React.ForwardRefExoticComponent<Props & React.RefAttributes<Element>>;
+
 export type ClassnameTypes = Pick<React.HTMLAttributes<HTMLElement>, 'className'>;
 
 export type ArrayStringTypes = string | string[];
+
+export type BreakpointsTypes = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
