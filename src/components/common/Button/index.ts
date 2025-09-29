ba4e6d555ed5204157963c4f17b46c250f957578
Bagas Ranggen <bagas.ranggen@gmail.com>
2025-09-29 19:53:03 +0700

    refactor: add button block component

--- src/components/common/Button/index.ts
+++ src/components/common/Button/index.ts
@@ -1,9 +1,15 @@
-import { RefComponent } from '@/libs/@types';
+import { Component, RefComponent } from '@/libs/@types';
 
 import Base, { BaseTypes } from '@/components/common/Button/Base';
+import Block, { BlockTypes } from '@/components/common/Button/Block';
 
 export type * from '@/components/common/Button/Base';
+export type * from '@/components/common/Button/Block';
 
-export type ButtonComposition = {};
+export type ButtonComposition = {
+    Block: Component<BlockTypes>;
+};
 
-export default Object.assign<RefComponent<BaseTypes, HTMLAnchorElement | HTMLDivElement>, ButtonComposition>(Base, {});
+export default Object.assign<RefComponent<BaseTypes, HTMLAnchorElement | HTMLDivElement>, ButtonComposition>(Base, {
+    Block,
+});
--- src/components/pages/HomepageIndex/index.tsx
+++ src/components/pages/HomepageIndex/index.tsx
@@ -4,6 +4,9 @@ import { CARDS_OFFSET, HOMEPAGE_BANNER } from '@/libs/mock';
 
 import HomepageBanner from '@/components/pages/HomepageIndex/HomepageBanner';
 import Cards from '@/components/common/Cards';
+import Container from '@/components/common/Container';
+import Link from '@/components/common/Link';
+import Button from '@/components/common/Button';
 
 export type HomepageIndexTypes = {};
 
@@ -12,13 +15,23 @@ const HomepageIndex = ({}: HomepageIndexTypes): React.ReactElement => {
         <>
             <HomepageBanner {...HOMEPAGE_BANNER} />
 
-            <Cards.Offset
-                wrapper={{
-                    as: 'section',
-                    className: 'relative z-10 bg-porto-light py-15',
-                }}
-                items={CARDS_OFFSET}
-            />
+            <section className="relative z-10 bg-porto-light py-15">
+                <Cards.Offset
+                    // wrapper={{
+                    //     as: 'section',
+                    //     className: 'relative z-10 bg-porto-light py-15',
+                    // }}
+                    items={CARDS_OFFSET}
+                />
+
+                <Container className="mt-18 text-center">
+                    <Button.Block
+                        as="anchor"
+                        href="/works">
+                        View All Works
+                    </Button.Block>
+                </Container>
+            </section>
         </>
     );
 };
