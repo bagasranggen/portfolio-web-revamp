1aeff2fa83f9dc5dc1d63dd1ade5ff0817ea9cd8
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2025-09-30 16:22:57 +0700

    refactor: add banner text animation

--- src/components/common/Banner/Text/index.tsx
+++ src/components/common/Banner/Text/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Columns from '@/components/common/Columns';
 import Heading, { BaseTypes } from '@/components/common/Heading';
 import Container from '@/components/common/Container';
+import Animation from '@/components/common/Animation';
 
 export type TextTypes = {
     title: BaseTypes['children'];
@@ -11,29 +12,39 @@ export type TextTypes = {
 
 const Text = ({ title, description, children }: TextTypes): React.ReactElement => {
     return (
-        <Container
-            as="section"
-            className="banner--text">
-            <Columns className="justify-between">
-                <Columns.Column lg={2}>
-                    <Heading
-                        as="h1"
-                        className="banner__title">
-                        {children}
-                    </Heading>
-                </Columns.Column>
+        <Animation type="banner-text">
+            <Container
+                as="section"
+                className="banner--text">
+                <Columns className="justify-between">
+                    <Columns.Column lg={2}>
+                        <Animation order={1}>
+                            <Heading
+                                as="h1"
+                                className="banner__title">
+                                {children}
+                            </Heading>
+                        </Animation>
+                    </Columns.Column>
 
-                <Columns.Column lg={7}>
-                    <Heading
-                        as="h2"
-                        className="mt-18 heading heading--title">
-                        {title}
-                    </Heading>
+                    <Columns.Column lg={7}>
+                        <Animation order={2}>
+                            <Heading
+                                as="h2"
+                                className="mt-18 heading heading--title">
+                                {title}
+                            </Heading>
+                        </Animation>
 
-                    {description && <div className="banner__description">{description}</div>}
-                </Columns.Column>
-            </Columns>
-        </Container>
+                        {description && (
+                            <Animation order={3}>
+                                <div className="banner__description">{description}</div>
+                            </Animation>
+                        )}
+                    </Columns.Column>
+                </Columns>
+            </Container>
+        </Animation>
     );
 };
 
