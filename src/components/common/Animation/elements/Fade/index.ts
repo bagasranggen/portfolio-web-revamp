8b9c1fc831fb9817d284b2bcafb3669af0ef00d5
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2025-12-12 09:49:36 +0700

    refactor: add option to set fade direction on fade animation

--- src/components/common/Animation/elements/Fade/index.ts
+++ src/components/common/Animation/elements/Fade/index.ts
@@ -4,13 +4,15 @@ import { animate } from 'animejs';
 
 import { fadeAnimation, FadeAnimationProps } from '@/components/common/Animation/elements/Fade/fadeAnimation';
 
-export const Fade = ({ target }: AnimationElementProps) => {
+export type FadeProps = AnimationElementProps & Pick<FadeAnimationProps, 'y'>;
+
+export const Fade = ({ target, y = 30 }: FadeProps) => {
     animate(
         target,
         fadeAnimation({
             clearTarget: target,
             clearStyle: true,
-            y: 30,
+            y,
         })
     );
 };
