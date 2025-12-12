cdd16858bc1302c2433ae4d9edd063eeb6d35685
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2025-12-12 15:33:05 +0700

    refactor: add env variable

--- src/store/context/GlobalContext.tsx
+++ src/store/context/GlobalContext.tsx
@@ -1,19 +1,22 @@
 'use client';
 
-import React, { createContext, PropsWithChildren, useContext } from 'react';
+import React, { createContext, PropsWithChildren } from 'react';
 
 export type GlobalState = {
     isDev: boolean;
+    isMultiLanguage: boolean;
 };
 
 export const GlobalStateContext = createContext<GlobalState>({
     isDev: false,
+    isMultiLanguage: false,
 });
 
 export const GlobalStateContextProvider = ({ children }: PropsWithChildren) => {
     const isDev = process.env.NODE_ENV === 'development';
+    const isMultiLanguage = process.env.NEXT_PUBLIC_FF_MULTI_LANGUAGE === '1';
 
-    const defaultContext = { isDev };
+    const defaultContext = { isDev, isMultiLanguage };
 
     return <GlobalStateContext.Provider value={defaultContext}>{children}</GlobalStateContext.Provider>;
 };
