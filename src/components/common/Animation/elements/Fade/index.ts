4666d8366b8370565ed8c1831b15a0e1c5108735
Bagas Ranggen <bagas.ranggen@gmail.com>
2025-12-13 21:44:42 +0700

    refactor: add duration on fade animation

--- src/components/common/Animation/elements/Fade/index.ts
+++ src/components/common/Animation/elements/Fade/index.ts
@@ -4,15 +4,16 @@ import { animate } from 'animejs';
 
 import { fadeAnimation, FadeAnimationProps } from '@/components/common/Animation/elements/Fade/fadeAnimation';
 
-export type FadeProps = AnimationElementProps & Pick<FadeAnimationProps, 'y'>;
+export type FadeProps = AnimationElementProps & Pick<FadeAnimationProps, 'y' | 'opacityDuration'>;
 
-export const Fade = ({ target, y = 30 }: FadeProps) => {
+export const Fade = ({ target, y = 30, opacityDuration }: FadeProps) => {
     animate(
         target,
         fadeAnimation({
             clearTarget: target,
             clearStyle: true,
             y,
+            opacityDuration,
         })
     );
 };
