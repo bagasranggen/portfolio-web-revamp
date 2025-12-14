d149e8e3b491e60238bf702674096d6082b7a9f0
Bagas Ranggen <bagas.ranggen@gmail.com>
2025-12-14 11:52:12 +0700

    refactor: add option to set delay on fade animation

--- src/components/common/Animation/elements/Fade/index.ts
+++ src/components/common/Animation/elements/Fade/index.ts
@@ -4,9 +4,9 @@ import { animate } from 'animejs';
 
 import { fadeAnimation, FadeAnimationProps } from '@/components/common/Animation/elements/Fade/fadeAnimation';
 
-export type FadeProps = AnimationElementProps & Pick<FadeAnimationProps, 'y' | 'opacityDuration'>;
+export type FadeProps = AnimationElementProps & Pick<FadeAnimationProps, 'y' | 'opacityDuration' | 'opacityDelay'>;
 
-export const Fade = ({ target, y = 30, opacityDuration }: FadeProps) => {
+export const Fade = ({ target, y = 30, opacityDuration, opacityDelay }: FadeProps) => {
     animate(
         target,
         fadeAnimation({
@@ -14,6 +14,7 @@ export const Fade = ({ target, y = 30, opacityDuration }: FadeProps) => {
             clearStyle: true,
             y,
             opacityDuration,
+            opacityDelay,
         })
     );
 };
