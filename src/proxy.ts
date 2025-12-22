2a1ba33221395f2d00b520f601a2b77e0631f6c6
Bagas Ranggen <bagas.ranggen@gmail.com>
2025-12-22 13:22:20 +0700

    refactor: get lang from header

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { LOCALES, LOCALES_DEFAULT, THEMES_COOKIE_KEY } from '@/libs/mock';
+
+import { LOCALES, LOCALES_DEFAULT, LOCALES_PROXY_KEY, THEMES_COOKIE_KEY, THEMES_PROXY_KEY } from '@/libs/mock';
+import { cleanArrayString } from '@/libs/utils';
 
 const IS_MULTI_LANGUAGE = process.env.NEXT_PUBLIC_FF_MULTI_LANGUAGE === '1';
 
@@ -15,9 +17,13 @@ export function proxy(request: NextRequest) {
         return NextResponse.redirect(nextUrl);
     }
 
+    const pathnameArr = cleanArrayString(pathname.split('/'));
+    const pathnameLocale = IS_MULTI_LANGUAGE && pathnameHasLocale ? pathnameArr?.[0] : LOCALES_DEFAULT;
+
     const theme = request.cookies.get(THEMES_COOKIE_KEY)?.value || 'light';
 
-    response.headers.set(THEMES_COOKIE_KEY, theme);
+    response.headers.set(THEMES_PROXY_KEY, theme);
+    response.headers.set(LOCALES_PROXY_KEY, pathnameLocale);
 
     return response;
 }
