ab9a53169551d6283d0b155000090d08ccaccfa5
Bagas Ranggen <bagas.ranggen@gmail.com>
2025-12-22 13:20:52 +0700

    refactor: refactoring layout component

---
+++ src/components/layout/Layout/index.tsx
@@ -0,0 +1,35 @@
+import React, { PropsWithChildren } from 'react';
+
+import '@/assets/styles/globals.css';
+
+import { ClassnameProps, LocaleProps, ThemesProps } from '@/libs/@types';
+
+import Navigation, { NavigationProps } from '@/components/layout/Navigation';
+import Footer, { FooterProps } from '@/components/layout/Footer';
+
+export type LayoutProps = {
+    lang?: LocaleProps;
+    theme?: ThemesProps;
+    navigation?: NavigationProps;
+    footer?: FooterProps;
+} & (PropsWithChildren & ClassnameProps);
+
+const Layout = ({ lang, theme, className, navigation, footer, children }: LayoutProps): React.ReactElement => {
+    return (
+        <>
+            <html
+                lang={lang}
+                data-theme={theme}>
+                <body {...(className ? { className } : {})}>
+                    {navigation && <Navigation {...navigation} />}
+
+                    {children}
+
+                    {footer && <Footer {...footer} />}
+                </body>
+            </html>
+        </>
+    );
+};
+
+export default Layout;
